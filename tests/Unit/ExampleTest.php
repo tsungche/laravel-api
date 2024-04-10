a80e62e21f5ebbe9f6faa64fe04afb227e3935c3
TTT <TTT@example.com>
2024-04-11 01:49:46 +0800

    add code

---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * A basic test example.
+     */
+    public function test_that_true_is_true(): void
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testBasicTest()
+    {
+        $res = $this->get('/');
+        $res->assertStatus(200);
+    }
+}
